9e06e01c95c2fb1ccbda0d8c06aefee50368ac90
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-07-15 17:07:54 +0300

    Snake logic is done
    I was not able at the moment to have a variable speed for the snake

---
+++ Snake/Assets/Scripts/SnakeLogic.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeLogic : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+
+    private void Update()
+    {
+        InputCheck();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveSnake();
+    }
+
+    private void MoveSnake()
+    {
+        transform.position = new Vector3(
+                Mathf.Round(transform.position.x) + _direction.x,
+                Mathf.Round(transform.position.y) + _direction.y,
+                0.0f
+            );
+    }
+    private void InputCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            _direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            _direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _direction = Vector2.right;
+        }
+    }
+}
