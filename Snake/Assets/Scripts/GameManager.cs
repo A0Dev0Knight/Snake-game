9201571dc5611255e09544397ec32547fff20d6d
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-07-16 19:47:47 +0300

    High score now displays on the pause menu

--- Snake/Assets/Scripts/GameManager.cs
+++ Snake/Assets/Scripts/GameManager.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
     GameObject PauseMenu;
+
+    [SerializeField]
+    Text HighScoreText;
 
     bool _isPaused = false;
 
@@ -20,6 +24,7 @@ public class GameManager : MonoBehaviour
     }
     private void PauseGame()
     {
+        HighScoreText.text = "Your high score is: " + PlayerPrefs.GetInt("HighScore", 0).ToString();
         if ((Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Space)) && _isPaused == false)
         {
             _isPaused = true;
